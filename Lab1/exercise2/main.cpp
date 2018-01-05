4a06da01322344c1428cad45f1faae68b6a41d4f
csw112358 <33767455+csw112358@users.noreply.github.com>
2018-01-05 10:55:19 -0600

    Add files via upload

---
+++ Lab1/exercise2/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(int argc, char** argv)
+{
+	std::cout << "My name is Chris Watkins.\nI am a physics major.\nMy hobbies are:\n\tReading\n\tCoding\n \tInvesting\nGoodbye" << std::endl;
+}
